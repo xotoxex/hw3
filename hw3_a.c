08cdbe0f3fb0b024365d84c0d66823435ec4dfd4
xotoxex <kotokek24@gmail.com>
2026-04-24 11:34:39 +0500

    2

--- hw3_a.c
+++ hw3_a.c
@@ -2,16 +2,24 @@
 
 static int var_1;
 
-int var_2;
+int var_2 = 67;
 
-int var_3;
+long long var_3;
 
 void foo(void);
 void bar(void);
 
 int main() {
+    var_1+=var_1;
+    var_2+=var_2;
+    var_3+=var_3;
+    for(int i = var_1; i < var_2; i++){
+        var_1+=var_1;
+        var_2+=var_2;
+        var_3+=var_3;
+    }
+    puts("20252028");
     foo();
     bar();
-    puts("20252028");
     return 0;
 }
