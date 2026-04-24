ca49228c7b0aa7efb1a18727bff5768d9bf09fe9
xotoxex <kotokek24@gmail.com>
2026-04-24 12:13:02 +0500

    20

--- hw3_b.c
+++ hw3_b.c
@@ -1,21 +1,25 @@
 #include <math.h>
-
-static int var_5 = 67;
-
+extern int var_2;
 int my_int[3] = {6,7,0};
-
+static int var_5 = 67;
 int var_3;
 
-extern int var_2;
-
 void foo(void) {
-    static int var_3_local;
-    var_3_local = var_3_local + 67;
+    static int var_3;
+    var_3 += var_2;
 }
 
 void bar(void) {
-    var_2 = var_2 + 67;
-    sqrt(16);
+    double math = sqrt((double)var_2);
+    (void)math;
+    int i;
+    for (i = 0; i < my_int[0]; i++) {
+        my_int[1] += i;
+        my_int[2] -= i;
+        var_5 += i;
+        var_5 -= my_int[1];
+        var_5 *= my_int[2];
+    }
 }
 
 void foo_bar(void) {
