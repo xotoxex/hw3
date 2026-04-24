5c0baa8c9cfe221d4ea7de6af13cd3b0d3cd9956
xotoxex <kotokek24@gmail.com>
2026-04-24 11:48:18 +0500

    10

--- hw3_a.c
+++ hw3_a.c
@@ -12,10 +12,8 @@ void bar(void);
 int main() {
     var_1 = var_1 + var_3;
     var_2++;
-    var_3;
-    var_2;
-    
-    
+    double var_4;
+    int var_5;
     puts("20252028");
     foo();
     bar();
